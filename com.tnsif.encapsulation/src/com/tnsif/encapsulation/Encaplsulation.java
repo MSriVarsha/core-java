9a0b854d15bdb5a2cfb50e55f8fc3bab9bb8e689
MSriVarsha <varshamamu6@gmail.com>
2025-01-30 20:26:56 +0530

    Second Commit

---
+++ com.tnsif.encapsulation/src/com/tnsif/encapsulation/Encaplsulation.java
@@ -0,0 +1,13 @@
+package com.tnsif.encapsulation;
+
+public class Encaplsulation {
+
+	int serialnumber;
+	String name;
+	int age;
+	public void show()
+	{
+		System.out.println("serialnumber:"+serialnumber+" name:"+name+" age:"+age);
+		// TODO Auto-generated method stub
+    }
+}
---
+++ com.tnsif.encapsulation/src/com/tnsif/encapsulation/Encapsulationdemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulationdemo {
+	public static void main(String[] args)
+	{
+	Encaplsulation e=new Encaplsulation();
+		e.serialnumber=9;
+		e.name="tarun";
+		e.age=23;
+		e.show();
+	}
+
+}
