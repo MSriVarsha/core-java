abb036e1bc61855a28059d14246585e3f40ef203
MSriVarsha <varshamamu6@gmail.com>
2025-02-28 15:32:40 +0530

    First Program

--- corejava/src/com/tnsif/testing/Parameterized.java
+++ corejava/src/com/tnsif/testing/Parameterized.java
@@ -8,9 +8,9 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class Parameterized {
 	
 	@ParameterizedTest
-	@ValueSource(strings={"call","bali","kali"})
+	@ValueSource(strings == {"call","bali","kali"})
 	void endswithI(String str) {
 		assertTrue(str.endsWith("i"));
 	}
 
-}
+	}
